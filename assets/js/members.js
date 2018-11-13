464a47ab3cf4479c89849fe03acd2ea0be08d2ae
Hackl0us <ask@hackl0us.com>
2018-11-13 21:27:45 +0800

    Added members.js: List all info of group members
    Called by Vue.js

---
+++ assets/js/members.js
@@ -0,0 +1,93 @@
+let vm = new Vue({
+    el: "#app",
+    data: {
+        manager_list: [{
+                avatar: "assets/img/hackl0us.jpg",
+                nickname: "Hackl0us",
+                position: "Founder / President",
+                works: "The Founder & President of the Apple User Group."
+            },
+            {
+                avatar: "assets/img/bearkevin.jpg",
+                nickname: "Kevin Wang",
+                position: "Ambassador",
+                works: "Provide the latest policies & news of Apple."
+            },
+            {
+                avatar: "assets/img/kingsun.jpg",
+                nickname: "Kingsun",
+                position: "Admin",
+                works: "Administrator of the group."
+            },
+            {
+                avatar: "assets/img/chrisyu.jpg",
+                nickname: "Chris Yu",
+                position: "Admin",
+                works: "Administrator of the group."
+            },
+            {
+                avatar: "assets/img/cnblacklistr.jpg",
+                nickname: "CNBlackListR",
+                position: "Admin (Robot)",
+                works: "A robot for blocking ADs, NSFW, robots, etc. automatically"
+            }
+        ],
+        core_list: [{
+                avatar: "assets/img/itumbledsea.jpg",
+                nickname: "iTumbledSea",
+                position: "ACTC / Author",
+                works: "Let's Talk iOS 9, Let's Talk iPhone"
+            },
+            {
+                avatar: "assets/img/benhuang.jpg",
+                nickname: "Ben Huang",
+                position: "Former Apple Genius",
+                works: "Apple Retail Store"
+            },
+            {
+                avatar: "assets/img/hackl0us.jpg",
+                nickname: "Hackl0us",
+                position: "Developer",
+                works: "Econom1ze / SS-Rule-Snippet / SS-Rule-Generator"
+            },
+            {
+                avatar: "assets/img/yicheng.jpg",
+                nickname: "YiCheng",
+                position: "Apple Devloper",
+                works: "ClashX"
+            },
+            {
+                avatar: "assets/img/o1xhack.jpg",
+                nickname: "o1xhack",
+                position: "Apple Devloper",
+                works: "Coffee time"
+            },
+            {
+                avatar: "assets/img/sunbelife.jpg",
+                nickname: "Sun Belife",
+                position: "Devloper",
+                works: "iBeta 尝鲜派"
+            },
+            {
+                avatar: "assets/img/tommysun.jpg",
+                nickname: "Tommy Sun",
+                position: "Blogger",
+                works: "@应用派小队"
+            },
+            {
+                avatar: "assets/img/lhie1.jpg",
+                nickname: "lhie1",
+                position: "Maintainer",
+                works: "Surge Rule"
+            },
+            {
+                avatar: "assets/img/u+7a7a.jpg",
+                nickname: "🈳️ U+7A7A 🔰",
+                position: "Maintainer",
+                works: "@Newbie_Guide / @TelePlanting Channel"
+            }
+        ]
+    },
+    methods: {},
+    mounted() {}
+});
